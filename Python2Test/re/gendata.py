3ea4f3d59a6cce85388d8a0cf1c3b9df49b2701a
koinhug@126.com <koinhug@126.com>
2016-08-12 14:41:23 +0800

    gendata.py

---
+++ Python2Test/re/gendata.py
@@ -0,0 +1,20 @@
+from random import randint, choice
+from string import lowercase
+from time import ctime
+
+doms = ('com', 'edu', 'net', 'org', 'gov')
+
+for i in range(randint(5, 10)):
+    dtint = randint(0, 1000000000)
+    dtstr = ctime(dtint)
+    shorter = randint(4, 7)
+    em = ''
+    for j in range(shorter):
+        em += choice(lowercase)
+
+    longer = randint(shorter, 12)
+    dn = ''
+    for j in range(longer):
+        dn += choice(lowercase)
+
+    print '%s::%s@%s.%s::%d-%d-%d' % (dtstr, em, dn, choice(doms), dtint, shorter, longer)
