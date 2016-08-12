29c415683e064c5e32eefafeddedf6d9a342fe4e
koinhug@126.com <koinhug@126.com>
2016-08-12 12:47:08 +0800

    re test

--- Python3Test/ReTest.py
+++ Python3Test/ReTest.py
@@ -2,7 +2,17 @@ import re
 
 # m = re.match('foo', 'foo')
 # m = re.match('foo', 'seafood')
-m = re.search('foo', 'seafood')
-if m is not None:
-    print(m.group())
+# m = re.search('foo', 'seafood')
+# m = re.match('.abc', ' abc')
+# if m is not None:
+#     print(m.group())
 
+# pattern = '\w+@\w+\.com'
+# print(re.match(pattern, 'nobody@gmail.com').group())
+
+# pattern = '\w+@(\w+\.)?\w+\.com'
+# print(re.match(pattern, 'nobody@www.gmail.com').group())
+# print(re.match(pattern, 'nobody@gmail.com').group())
+
+# print(re.search('cc', 'ccc').group())
+# print(re.findall('cc', 'ccc'))
